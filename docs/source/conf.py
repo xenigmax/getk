caeae38ab87845704a7f17a1964137d7e225511b
Jongkyu Kim <xenigmax@gmail.com>
2017-02-16 18:15:57 +0100

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -132,8 +132,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'getk.tex', project,
-     author, 'manual'),
+    (master_doc, 'getk.tex', u'Read The Docs Documentation',
+     u'Eric', 'manual'),
 ]
 
 
